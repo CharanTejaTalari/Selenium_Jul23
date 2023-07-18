4634942f160c792764dc9ce641ce97de32eff169
CharanTejaTalari <talaricharanteja@gmail.com>
2023-07-18 22:57:34 +0530

    Create GenerateRandomNumbersAndString.java

---
+++ GenerateRandomNumbersAndString.java
@@ -0,0 +1,40 @@
+package JavaP;
+
+//import java.util.Random;
+
+//import net.bytebuddy.utility.RandomString;
+
+public class GenerateRandomNumbersAndString {
+
+	public static void main(String[] args) {
+		//Approach-1
+		/*//By using random class
+		Random ran = new Random();
+		
+		int ran_int = ran.nextInt(10);
+		System.out.println(ran_int);
+		
+		double ran_Double = ran.nextDouble();
+		System.out.println(ran_Double);
+		
+		/*float ran_Float = ran.nextFloat();
+		System.out.println(ran_Float);
+		
+		String ran_String = ran.toString();
+		System.out.println(ran_String);
+		*/
+		
+		//Approach-2 it will generate decimals
+		/*//By using math class and random method
+		System.out.println(Math.random());
+		*/
+		
+		//Approach-3
+		//By using Apache commons-lang API - it will generate random numbers and strings
+		
+		
+		
+		
+	}
+
+}
